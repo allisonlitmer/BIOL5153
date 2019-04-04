35e92f6494c5f75315bfe6951c165df6e04a7ed2
Allison Litmer <arlitmer@uark.edu>
2019-04-04 15:14:06 -0500

    second draft, added argparse

--- parsegff.py
+++ parsegff.py
@@ -1,24 +1,35 @@
+#! /usr/bin/env python3
 
-#!/bin/bash
+#import the program argparse
 
-#read the files in, open files
+import argparse
 
-fasta = "watermelon.fsa"
-fasta_file = open(fasta, "r")
-genome = fasta_file.read()
+#cerate an ArgumentParser object ('parser') that will hold all the info necessary
+#to parse the command line
 
-filename = "watermelon.gff"
-gff = open(filename, "r")
+parser = argparse.ArgumentParser(description = "generates a genome sequence and the features")
 
+# add a positional argument
+
+parser.add_argument("fasta", help="the name of the job" ,type=argparse.FileType("r"))
+parser.add_argument("gff", help="the features of the genome",type=argparse.FileType("r"))
+
+#parse the command line arguments so you can access parse arguments
+
+args = parser.parse_args()
+
+#genome = open(args.fasta, "r")
+#features = open(args.gff, "r")
 
 #reading line by line
 
-for line in gff:
+for line in args.gff:
     #skip blanks
 
     #remove line breaks
     line = line.rstrip('\n')
-    
+    print(line)
+
 #split based on tabs
 #sequence, source, feature, begin, end, length, strand, phase, attributes = line.split('\t')
 
@@ -26,8 +37,7 @@ for line in gff:
 #print(fields[3], fields[4])
 
 #extract the DNA seqeuence from the genome for this feature (corresponds to the coordinates in GFF)
-substring = genome[378929:379011]
-
+substring = args.fasta[378929:379011]
 
 #print the DNA sequence for this feature
 print(substring)
